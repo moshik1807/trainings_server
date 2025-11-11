0bcb72a55c05bb35c79ed7f5007d150254f82928
moshik <moshik0527669169@gmail.com>
2025-11-11 11:14:49 +0200

    Adding pictures

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 import express from "express"
+import path from "path";
 import cors from "cors"
+import { fileURLToPath } from "url"  
 import routTrainees from "./routs/routTrainees.js"
 import routTrainings from "./routs/routTrainings.js"
 import routTrainers from "./routs/routTrainers.js"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 const port = 3000
 
 const app = express()
 app.use(cors())
 app.use(express.json());
+app.use('/images', express.static(path.join(__dirname, 'public/images')))
 
 app.use("/trainees", routTrainees);
 
